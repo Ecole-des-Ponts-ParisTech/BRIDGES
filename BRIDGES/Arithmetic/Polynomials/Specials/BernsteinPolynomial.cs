59f7de1b1b51ad5ed79c7d92bb8aa9b64510577b
nrmontagne <nicolas.montagne@enpc.fr>
2022-07-26 15:30:52 +0200

    WIP Bernstein and Multivariate Polynomials

---
+++ BRIDGES/Arithmetic/Polynomials/Specials/BernsteinPolynomial.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRIDGES.Arithmetic.Polynomials.Specials
+{
+    public class BernsteinPolynomial : Polynomial
+    {
+    }
+}
