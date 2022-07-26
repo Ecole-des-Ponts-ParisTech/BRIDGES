12e33e9a2b0f27df101ba552cd09052c616c5aeb
nrmontagne <nicolas.montagne@enpc.fr>
2022-07-26 15:37:53 +0200

    WIP NURBS Curve

---
+++ BRIDGES/Geometry/Euclidean3D/Manifold_1D/NURBSCurve.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRIDGES.Geometry.Euclidean3D
+{
+
+    /// <summary>
+    /// Class defining a generic NURBS Curve in three-dimensional euclidean space.
+    /// </summary>
+    public class NURBSCurve : Kernel.NURBSCurve<Point>
+    {
+        /// <inheritdoc/>
+        public NURBSCurve(int degree, Point[] controlPoints) 
+            : base(degree, controlPoints)
+        {
+        }
+
+        /// <inheritdoc/>
+        public NURBSCurve(int degree, double[] knotVector, Point[] controlPoints, double[] weights)
+            : base(degree, knotVector, controlPoints, weights)
+        {
+
+        }
+    }
+}
